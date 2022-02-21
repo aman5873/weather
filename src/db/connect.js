4374eb3d08ae8d73fa646f28ddcbff9d76b25169
aman <aman5873@outlook.com>
2022-02-21 17:01:39 +0530

    modified db

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-// const DB_URL = 'mongodb+srv://aman:aman@cluster0.4s7yu.mongodb.net/mernregister?retryWrites=true&w=majority' // Atlas-Db
-const DB_URL = 'mongodb://localhost:27017/mernregister' // local-Db
+const DB_URL = 'mongodb+srv://aman:aman@cluster0.4s7yu.mongodb.net/mernregister?retryWrites=true&w=majority' // Atlas-Db
+// const DB_URL = 'mongodb://localhost:27017/mernregister' // local-Db
 mongoose.connect(DB_URL)
 .then(() => {
     console.log(`sucessfully connected to DB`);
